be62e5a68c3673977c8fb0e6293a2c41b1007cc8
LingFeng-bbben <bengy@126.com>
2024-01-04 01:22:27 +0000

    评论区

--- src/app/song/page.jsx
+++ src/app/song/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { PhotoProvider, PhotoView } from "react-photo-view";
 import "react-photo-view/dist/react-photo-view.css";
 import useSWR from "swr";
@@ -20,7 +20,10 @@ export default function Page() {
   const param = searchParams.get("id");
   return (
     <>
-      <div className="bg" style={{backgroundImage: `url(${apiroot3}/Image/${param})`}}></div>
+      <div
+        className="bg"
+        style={{ backgroundImage: `url(${apiroot3}/Image/${param})` }}
+      ></div>
       <div className="seprate"></div>
       <h1>
         <img
@@ -62,14 +65,19 @@ export default function Page() {
         placement="top-start"
         interactive={true}
       />
-      <SongInfo id={param} tippy={target}/>
+      <SongInfo id={param} tippy={target} />
+      <CommentSender songid={param} />
+      <CommentList songid={param}/>
     </>
   );
 }
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
-function SongInfo({id,tippy}){
-  const { data, error, isLoading } = useSWR(apiroot3 + "/SongList/" +id, fetcher);
+function SongInfo({ id, tippy }) {
+  const { data, error, isLoading } = useSWR(
+    apiroot3 + "/SongList/" + id,
+    fetcher
+  );
   if (error) return <div>failed to load</div>;
   if (isLoading) {
     return <div className="loading">Loading SongInfo...</div>;
@@ -105,7 +113,9 @@ function SongInfo({id,tippy}){
   };
 
   const shareSong = (props) => async () => {
-    await navigator.clipboard.writeText("https://majdata.net/song?id=" + props.id);
+    await navigator.clipboard.writeText(
+      "https://majdata.net/song?id=" + props.id
+    );
     toast.success("已复制到剪贴板", {
       position: "bottom-center",
       autoClose: 3000,
@@ -119,59 +129,57 @@ function SongInfo({id,tippy}){
   };
 
   const o = data;
-  return(
+  return (
     <div className="theList">
       <div className="songCard">
-          <CoverPic id={o.id} />
-          <div className="songInfo">
-            <Tippy content={o.title} singleton={tippy}>
-              <div className="songTitle" id={o.id}>
-                {o.title}
-              </div>
-            </Tippy>
-            <Tippy content={o.artist} singleton={tippy}>
-              <div className="songArtist">
-                {o.artist == "" || o.artist == null ? "-" : o.artist}
-              </div>
-            </Tippy>
-            <Tippy content={o.uploader + "@" + o.designer} singleton={tippy}>
-              <div className="songDesigner">
-                {o.uploader + "@" + o.designer}
-              </div>
-            </Tippy>
+        <CoverPic id={o.id} />
+        <div className="songInfo">
+          <Tippy content={o.title} singleton={tippy}>
+            <div className="songTitle" id={o.id}>
+              {o.title}
+            </div>
+          </Tippy>
+          <Tippy content={o.artist} singleton={tippy}>
+            <div className="songArtist">
+              {o.artist == "" || o.artist == null ? "-" : o.artist}
+            </div>
+          </Tippy>
+          <Tippy content={o.uploader + "@" + o.designer} singleton={tippy}>
+            <div className="songDesigner">{o.uploader + "@" + o.designer}</div>
+          </Tippy>
 
-            <Levels levels={o.levels} songid={o.id} />
-            <br />
-            <div
-              className="songLevel downloadButtonBox"
-              onClick={shareSong({ id: o.id })}
+          <Levels levels={o.levels} songid={o.id} />
+          <br />
+          <div
+            className="songLevel downloadButtonBox"
+            onClick={shareSong({ id: o.id })}
+          >
+            <svg
+              className="shareButton"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24"
+              viewBox="0 -960 960 960"
+              width="24"
             >
-              <svg
-                className="shareButton"
-                xmlns="http://www.w3.org/2000/svg"
-                height="24"
-                viewBox="0 -960 960 960"
-                width="24"
-              >
-                <path d="M720-80q-50 0-85-35t-35-85q0-7 1-14.5t3-13.5L322-392q-17 15-38 23.5t-44 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q23 0 44 8.5t38 23.5l282-164q-2-6-3-13.5t-1-14.5q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-23 0-44-8.5T638-672L356-508q2 6 3 13.5t1 14.5q0 7-1 14.5t-3 13.5l282 164q17-15 38-23.5t44-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-640q17 0 28.5-11.5T760-760q0-17-11.5-28.5T720-800q-17 0-28.5 11.5T680-760q0 17 11.5 28.5T720-720ZM240-440q17 0 28.5-11.5T280-480q0-17-11.5-28.5T240-520q-17 0-28.5 11.5T200-480q0 17 11.5 28.5T240-440Zm480 280q17 0 28.5-11.5T760-200q0-17-11.5-28.5T720-240q-17 0-28.5 11.5T680-200q0 17 11.5 28.5T720-160Zm0-600ZM240-480Zm480 280Z" />
-              </svg>
-            </div>
-            <div
-              className="songLevel downloadButtonBox"
-              onClick={downloadSong({ id: o.id, title: o.title })}
+              <path d="M720-80q-50 0-85-35t-35-85q0-7 1-14.5t3-13.5L322-392q-17 15-38 23.5t-44 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q23 0 44 8.5t38 23.5l282-164q-2-6-3-13.5t-1-14.5q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-23 0-44-8.5T638-672L356-508q2 6 3 13.5t1 14.5q0 7-1 14.5t-3 13.5l282 164q17-15 38-23.5t44-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-640q17 0 28.5-11.5T760-760q0-17-11.5-28.5T720-800q-17 0-28.5 11.5T680-760q0 17 11.5 28.5T720-720ZM240-440q17 0 28.5-11.5T280-480q0-17-11.5-28.5T240-520q-17 0-28.5 11.5T200-480q0 17 11.5 28.5T240-440Zm480 280q17 0 28.5-11.5T760-200q0-17-11.5-28.5T720-240q-17 0-28.5 11.5T680-200q0 17 11.5 28.5T720-160Zm0-600ZM240-480Zm480 280Z" />
+            </svg>
+          </div>
+          <div
+            className="songLevel downloadButtonBox"
+            onClick={downloadSong({ id: o.id, title: o.title })}
+          >
+            <svg
+              className="downloadButton"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24"
+              viewBox="0 -960 960 960"
+              width="24"
             >
-              <svg
-                className="downloadButton"
-                xmlns="http://www.w3.org/2000/svg"
-                height="24"
-                viewBox="0 -960 960 960"
-                width="24"
-              >
-                <path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z" />
-              </svg>
-            </div>
+              <path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z" />
+            </svg>
           </div>
         </div>
+      </div>
     </div>
   );
 }
@@ -276,4 +284,91 @@ function Levels({ levels, songid }) {
       </div>
     </div>
   );
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  if (typeof window !== "undefined") {
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == " ") {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+  }
+  return "";
+}
+
+function CommentSender({ songid }) {
+  const [comment, SetCommnet] = useState("");
+  const onSubmit = async() => {
+    const formData = new FormData()
+    formData.set('token',getCookie('token'))
+    formData.set('type','comment')
+    formData.set('content',comment)
+    const response = await fetch(apiroot3+'/Interact/'+songid, {
+      method: 'POST',
+      body: formData,
+    })
+    if (response.status ==200){
+      toast.success("评论成功")
+    }else if(response.status ==400){
+      toast.error("评论失败：空的哟？")
+    }else{
+      toast.error("评论失败：登录了吗？")
+    }
+  };
+  return (
+    <>
+      <div className="theList">
+        <p className="inputHint">评论</p>
+      </div>
+      <div className="theList">
+      <textarea
+        className="userinput commentbox"
+        type="text"
+        onChange={() => SetCommnet(event.target.value)}
+      />
+      <button
+        className="linkContent"
+        id="submitbutton"
+        type="button"
+        onClick={onSubmit}
+      >
+        发表
+      </button>
+    </div>
+    </>
+  );
+}
+
+function CommentList({songid}){
+  const { data, error, isLoading } = useSWR(
+    apiroot3 + "/Interact/" + songid,
+    fetcher,
+    { refreshInterval: 3000 }
+  );
+  if (error) return <div>failed to load</div>;
+  if (isLoading) {
+    return <div className="loading">Loading Comments...</div>;
+  }
+  if (data == "" || data == undefined) return <div>failed to load</div>;
+  const commentList = Object.entries(data.CommentsList).reverse();
+  console.log(commentList)
+  const objlist = commentList.map((o) => (
+    <div key={o[0]}>
+      <div className="CommentCard">
+        <p className="CommentUser">{o[0]}</p>
+        <p className="CommentContent">{o[1]}</p>
+      </div>
+    </div>
+  ))
+  return(<div className="theList">
+    {objlist}
+  </div>);
 }
