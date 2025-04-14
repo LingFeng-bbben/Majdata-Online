87768508060d81ab8e7984c8c454e3e4f97d8032
LingFeng-bbben <bengy@126.com>
2025-04-15 07:00:45 +0800

    feat: icon

--- src/app/userinfo.jsx
+++ src/app/userinfo.jsx
@@ -1,12 +1,44 @@
-'use client'
-import React from 'react';
-import useSWR from 'swr';
+"use client";
+import React from "react";
+import useSWR from "swr";
+import { apiroot3 } from "./apiroot";
 
-const fetcher = (url) => fetch(url,{mode:"cors",credentials: "include"}).then((res) => res.json())
-export default function UserInfo({apiroot}){
-const { data, error, isLoading } = useSWR(apiroot + "/account/info/", fetcher);
-  if(error) return <div className='linkContent'><a href='./login'>登录</a></div>
-  if(isLoading) return <div className='linkContent'><a href='./login'>...</a></div>
-  if(data.Username == undefined) return <div className='linkContent'><a href='./login'>登录</a></div>
-  return <div className='linkContent'><a href='./user'>{'用户:'+data.Username}</a></div>
+const fetcher = (url) =>
+  fetch(url, { mode: "cors", credentials: "include" }).then((res) =>
+    res.json()
+  );
+export default function UserInfo({ apiroot }) {
+  const { data, error, isLoading } = useSWR(
+    apiroot + "/account/info/",
+    fetcher
+  );
+  if (error)
+    return (
+      <div className="linkContent">
+        <a href="./login">登录</a>
+      </div>
+    );
+  if (isLoading)
+    return (
+      <div className="linkContent">
+        <a href="./login">...</a>
+      </div>
+    );
+  if (data.Username == undefined)
+    return (
+      <div className="linkContent">
+        <a href="./login">登录</a>
+      </div>
+    );
+  return (
+    <div className="linkContent">
+      <a href="./user">
+        <img
+          className="smallIcon"
+          src={apiroot3 + "/account/Icon?username=" + data.Username}
+        />
+        {data.Username}
+      </a>
+    </div>
+  );
 }
