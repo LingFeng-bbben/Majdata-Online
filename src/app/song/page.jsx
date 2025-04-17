9046b83311eccf0de0ddda93eb9214670d3d66a3
LingFeng-bbben <bengy@126.com>
2025-04-18 02:11:41 +0800

    feat: auto load majdata

--- src/app/song/page.jsx
+++ src/app/song/page.jsx
@@ -1,14 +1,14 @@
 "use client";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import useSWR from "swr";
-import {apiroot3} from "../apiroot";
-import Tippy, {useSingleton} from "@tippyjs/react";
+import { apiroot3 } from "../apiroot";
+import Tippy, { useSingleton } from "@tippyjs/react";
 import "tippy.js/dist/tippy.css";
-import {useSearchParams} from "next/navigation";
-import {toast, ToastContainer} from "react-toastify";
+import { useSearchParams } from "next/navigation";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import {downloadSong} from "../download";
-import {getLevelName, getComboState} from "../utils";
+import { downloadSong } from "../download";
+import { getLevelName, getComboState } from "../utils";
 import {
   CoverPic,
   Levels,
@@ -16,7 +16,7 @@ import {
   MajdataLogo,
   TagManageTagLauncher,
   TagManageWidget,
-  UserInfo
+  UserInfo,
 } from "../widgets";
 
 export default function Page() {
@@ -27,10 +27,10 @@ export default function Page() {
     <>
       <div
         className="bg"
-        style={{backgroundImage: `url(${apiroot3}/maichart/${param}/image)`}}
+        style={{ backgroundImage: `url(${apiroot3}/maichart/${param}/image)` }}
       ></div>
       <div className="seprate"></div>
-      <MajdataLogo/>
+      <MajdataLogo />
       <div className="links">
         <div className="linkContent">
           <a href="/">主页</a>
@@ -48,36 +48,35 @@ export default function Page() {
           theme="dark"
         />
         {/* <div className='linkContent'><a href='./contest'>MMFC 6th</a></div> */}
-        <UserInfo/>
+        <UserInfo />
       </div>
 
-      <Majdata/>
       <Tippy
         singleton={source}
         animation="fade"
         placement="top-start"
         interactive={true}
       />
-      <SongInfo id={param} tippy={target}/>
-      <LikeSender songid={param}/>
+      <SongInfo id={param} tippy={target} />
+      <LikeSender songid={param} />
       <div className="hr-solid"></div>
-      <ScoreList songid={param}/>
+      <ScoreList songid={param} />
       <div className="hr-solid"></div>
-      <CommentSender songid={param}/>
-      <CommentList songid={param}/>
-      <img className="footerImage" loading="lazy" src={"/bee.webp"} alt=""/>
+      <CommentSender songid={param} />
+      <CommentList songid={param} />
+      <img className="footerImage" loading="lazy" src={"/bee.webp"} alt="" />
     </>
   );
 }
 
 const fetcher = (url) =>
-  fetch(url, {mode: "cors", credentials: "include"}).then((res) =>
+  fetch(url, { mode: "cors", credentials: "include" }).then((res) =>
     res.json()
   );
 
-function SongInfo({id, tippy}) {
+function SongInfo({ id, tippy }) {
   const tagButtonRef = useState();
-  const {data, error, isLoading} = useSWR(
+  const { data, error, isLoading } = useSWR(
     apiroot3 + "/maichart/" + id + "/summary",
     fetcher
   );
@@ -92,7 +91,7 @@ function SongInfo({id, tippy}) {
   }
 
   const OnDownloadClick = (params) => async () => {
-    await downloadSong({id: params.id, title: params.title, toast: toast});
+    await downloadSong({ id: params.id, title: params.title, toast: toast });
   };
 
   const shareSong = (props) => async () => {
@@ -110,133 +109,136 @@ function SongInfo({id, tippy}) {
       theme: "dark",
     });
   };
-
   const o = data;
+  const firstNonEmptyIndex = o.levels.findIndex((level) => level !== "");
+
   return (
-    <div className="theList">
-      <div className="songCard">
-        <CoverPic id={o.id}/>
-        <div className="songInfo">
-          <Tippy content={o.title} singleton={tippy}>
-            <div className="songTitle" id={o.id}>
-              {o.title}
-            </div>
-          </Tippy>
-          <Tippy content={o.artist} singleton={tippy}>
-            <div className="songArtist">
-              {o.artist === "" || o.artist == null ? "-" : o.artist}
-            </div>
-          </Tippy>
-          <Tippy content={o.uploader + "@" + o.designer} singleton={tippy}>
-            <div className="songDesigner">
-              <a href={"/space?id=" + o.uploader}>
-                <img
-                  className="smallIcon"
-                  src={apiroot3 + "/account/Icon?username=" + o.uploader}
-                  alt={o.uploader}
-                />
-                {o.uploader + "@" + o.designer}
-              </a>
-            </div>
-          </Tippy>
+    <div>
+      <Majdata
+        songid={o.id}
+        apiroot={apiroot3}
+        level={"lv" + firstNonEmptyIndex}
+      />
+      <div className="theList">
+        <div className="songCard">
+          <CoverPic id={o.id} />
+          <div className="songInfo">
+            <Tippy content={o.title} singleton={tippy}>
+              <div className="songTitle" id={o.id}>
+                {o.title}
+              </div>
+            </Tippy>
+            <Tippy content={o.artist} singleton={tippy}>
+              <div className="songArtist">
+                {o.artist == "" || o.artist == null ? "-" : o.artist}
+              </div>
+            </Tippy>
+            <Tippy content={o.uploader + "@" + o.designer} singleton={tippy}>
+              <div className="songDesigner">
+                <a href={"/space?id=" + o.uploader}>
+                  <img
+                    className="smallIcon"
+                    src={apiroot3 + "/account/Icon?username=" + o.uploader}
+                    alt={o.uploader}
+                  />
+                  {o.uploader + "@" + o.designer}
+                </a>
+              </div>
+            </Tippy>
 
-          <Levels levels={o.levels} songid={o.id} isPlayer={true}/>
-          <br/>
-          <div
-            className="songLevel downloadButtonBox"
-            onClick={shareSong({id: o.id})}
-          >
-            <svg
-              className="shareButton"
-              xmlns="http://www.w3.org/2000/svg"
-              height="24"
-              viewBox="0 -960 960 960"
-              width="24"
+            <Levels levels={o.levels} songid={o.id} isPlayer={true} />
+            <br />
+            <div
+              className="songLevel downloadButtonBox"
+              onClick={shareSong({ id: o.id })}
             >
-              <path
-                d="M720-80q-50 0-85-35t-35-85q0-7 1-14.5t3-13.5L322-392q-17 15-38 23.5t-44 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q23 0 44 8.5t38 23.5l282-164q-2-6-3-13.5t-1-14.5q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-23 0-44-8.5T638-672L356-508q2 6 3 13.5t1 14.5q0 7-1 14.5t-3 13.5l282 164q17-15 38-23.5t44-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-640q17 0 28.5-11.5T760-760q0-17-11.5-28.5T720-800q-17 0-28.5 11.5T680-760q0 17 11.5 28.5T720-720ZM240-440q17 0 28.5-11.5T280-480q0-17-11.5-28.5T240-520q-17 0-28.5 11.5T200-480q0 17 11.5 28.5T240-440Zm480 280q17 0 28.5-11.5T760-200q0-17-11.5-28.5T720-240q-17 0-28.5 11.5T680-200q0 17 11.5 28.5T720-160Zm0-600ZM240-480Zm480 280Z"/>
-            </svg>
-          </div>
-          <div
-            className="songLevel downloadButtonBox"
-            onClick={OnDownloadClick({id: o.id, title: o.title})}
-          >
-            <svg
-              className="downloadButton"
-              xmlns="http://www.w3.org/2000/svg"
-              height="24"
-              viewBox="0 -960 960 960"
-              width="24"
+              <svg
+                className="shareButton"
+                xmlns="http://www.w3.org/2000/svg"
+                height="24"
+                viewBox="0 -960 960 960"
+                width="24"
+              >
+                <path d="M720-80q-50 0-85-35t-35-85q0-7 1-14.5t3-13.5L322-392q-17 15-38 23.5t-44 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q23 0 44 8.5t38 23.5l282-164q-2-6-3-13.5t-1-14.5q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-23 0-44-8.5T638-672L356-508q2 6 3 13.5t1 14.5q0 7-1 14.5t-3 13.5l282 164q17-15 38-23.5t44-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-640q17 0 28.5-11.5T760-760q0-17-11.5-28.5T720-800q-17 0-28.5 11.5T680-760q0 17 11.5 28.5T720-720ZM240-440q17 0 28.5-11.5T280-480q0-17-11.5-28.5T240-520q-17 0-28.5 11.5T200-480q0 17 11.5 28.5T240-440Zm480 280q17 0 28.5-11.5T760-200q0-17-11.5-28.5T720-240q-17 0-28.5 11.5T680-200q0 17 11.5 28.5T720-160Zm0-600ZM240-480Zm480 280Z" />
+              </svg>
+            </div>
+            <div
+              className="songLevel downloadButtonBox"
+              onClick={OnDownloadClick({ id: o.id, title: o.title })}
             >
-              <path
-                d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/>
-            </svg>
+              <svg
+                className="downloadButton"
+                xmlns="http://www.w3.org/2000/svg"
+                height="24"
+                viewBox="0 -960 960 960"
+                width="24"
+              >
+                <path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z" />
+              </svg>
+            </div>
+            <TagManageWidget ref={tagButtonRef} songid={o.id}></TagManageWidget>
           </div>
-          <TagManageWidget ref={tagButtonRef} songid={o.id}></TagManageWidget>
         </div>
-      </div>
 
-      <div className="uploadDate"></div>
+        <div className="uploadDate"></div>
 
-      <div className="uploadMeta">
-        <div className="uploadMetaRow">
-          <div className="uploadMetaLabel">Time:</div>
-          <div className="uploadMetaContent">{o.timestamp}</div>
-        </div>
-        <div className="uploadMetaRow">
-          <div className="uploadMetaLabel">ID:</div>
-          <div className="uploadMetaContent">{o.id}</div>
-        </div>
-        <div className="uploadMetaRow">
-          <div className="uploadMetaLabel">HASH:</div>
-          <div className="uploadMetaContent">{o.hash}</div>
-        </div>
-        <div className="uploadMetaRow">
-          <div className="uploadMetaLabel">Tags:</div>
-          <div className="uploadMetaContent tagList">
-            {(
-               o.tags || o.publicTags
-             ) && (
-               o.tags.length > 0 || o.publicTags.length > 0
-             ) ? (
-               <>
-                 {
-                   o.tags.map((tag, index) => (
-                     <Tippy content="搜索标签" key={index}>
-                  <span
-                    className="tag"
-                    onClick={() => {
-                      localStorage.setItem("search", tag);
-                      window.location.href = "/";
-                    }}
-                  >
-                    {tag}
-                  </span>
-                     </Tippy>
-                   ))
-                 }
-                 {o.publicTags?.map((tag, index) => (
-                   <Tippy content="搜索标签" key={index}>
-                  <span
-                    className="tag tagPublic"
-                    onClick={() => {
-                      localStorage.setItem("search", tag);
-                      window.location.href = "/";
-                    }}
-                  >
-                    {tag}
-                  </span>
-                   </Tippy>
-                 ))}
-               </>
-             ) : (
-               <span style={{color: "#999", fontStyle: "italic"}}>
-                暂无标签
-              </span>
-             )}
-            <TagManageTagLauncher onClick={() => {
-              tagButtonRef.current?.toggleWindow();
-            }}/>
+        <div className="uploadMeta">
+          <div className="uploadMetaRow">
+            <div className="uploadMetaLabel">Time:</div>
+            <div className="uploadMetaContent">{o.timestamp}</div>
+          </div>
+          <div className="uploadMetaRow">
+            <div className="uploadMetaLabel">ID:</div>
+            <div className="uploadMetaContent">{o.id}</div>
+          </div>
+          <div className="uploadMetaRow">
+            <div className="uploadMetaLabel">HASH:</div>
+            <div className="uploadMetaContent">{o.hash}</div>
+          </div>
+          <div className="uploadMetaRow">
+            <div className="uploadMetaLabel">Tags:</div>
+            <div className="uploadMetaContent tagList">
+              {(o.tags || o.publicTags) &&
+              (o.tags.length > 0 || o.publicTags.length > 0) ? (
+                <>
+                  {o.tags.map((tag, index) => (
+                    <Tippy content="搜索标签" key={index}>
+                      <span
+                        className="tag"
+                        onClick={() => {
+                          localStorage.setItem("search", tag);
+                          window.location.href = "/";
+                        }}
+                      >
+                        {tag}
+                      </span>
+                    </Tippy>
+                  ))}
+                  {o.publicTags?.map((tag, index) => (
+                    <Tippy content="搜索标签" key={index}>
+                      <span
+                        className="tag tagPublic"
+                        onClick={() => {
+                          localStorage.setItem("search", tag);
+                          window.location.href = "/";
+                        }}
+                      >
+                        {tag}
+                      </span>
+                    </Tippy>
+                  ))}
+                </>
+              ) : (
+                <span style={{ color: "#999", fontStyle: "italic" }}>
+                  暂无标签
+                </span>
+              )}
+              <TagManageTagLauncher
+                onClick={() => {
+                  tagButtonRef.current?.toggleWindow();
+                }}
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -244,8 +246,8 @@ function SongInfo({id, tippy}) {
   );
 }
 
-function LikeSender({songid}) {
-  const {data, error, isLoading, mutate} = useSWR(
+function LikeSender({ songid }) {
+  const { data, error, isLoading, mutate } = useSWR(
     apiroot3 + "/maichart/" + songid + "/interact",
     fetcher
   );
@@ -279,11 +281,9 @@ function LikeSender({songid}) {
     if (response.status == 200) {
       toast.success("点赞成功");
       mutate();
-    }
-    else if (response.status == 400) {
+    } else if (response.status == 400) {
       toast.error("点赞失败：登录了吗？");
-    }
-    else {
+    } else {
       toast.error("点赞失败：登录了吗？");
     }
   };
@@ -295,7 +295,7 @@ function LikeSender({songid}) {
           id="submitbuttonlike"
           type="button"
           onClick={onSubmit}
-          style={{background: data.IsLiked ? "green" : ""}}
+          style={{ background: data.IsLiked ? "green" : "" }}
         >
           <svg
             className="commentIco"
@@ -303,10 +303,9 @@ function LikeSender({songid}) {
             height="24"
             viewBox="0 -960 960 960"
             width="24"
-            style={{width: "24px", height: "24px"}}
+            style={{ width: "24px", height: "24px" }}
           >
-            <path
-              d="M720-120H280v-520l280-280 50 50q7 7 11.5 19t4.5 23v14l-44 174h258q32 0 56 24t24 56v80q0 7-2 15t-4 15L794-168q-9 20-30 34t-44 14Zm-360-80h360l120-280v-80H480l54-220-174 174v406Zm0-406v406-406Zm-80-34v80H160v360h120v80H80v-520h200Z"/>
+            <path d="M720-120H280v-520l280-280 50 50q7 7 11.5 19t4.5 23v14l-44 174h258q32 0 56 24t24 56v80q0 7-2 15t-4 15L794-168q-9 20-30 34t-44 14Zm-360-80h360l120-280v-80H480l54-220-174 174v406Zm0-406v406-406Zm-80-34v80H160v360h120v80H80v-520h200Z" />
           </svg>
         </button>
         <p>{likecount}</p>
@@ -314,10 +313,9 @@ function LikeSender({songid}) {
           className="commentIco"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 -960 960 960"
-          style={{width: "24px", height: "24px", margin: "15px"}}
+          style={{ width: "24px", height: "24px", margin: "15px" }}
         >
-          <path
-            d="m380-300 280-180-280-180v360ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
+          <path d="m380-300 280-180-280-180v360ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
         </svg>
         <p>{playcount}</p>
       </div>
@@ -328,7 +326,8 @@ function LikeSender({songid}) {
               <img
                 className="smallIcon"
                 src={apiroot3 + "/account/Icon?username=" + o}
-                alt={o}/>
+                alt={o}
+              />
               {o}
             </p>
           </a>
@@ -338,7 +337,7 @@ function LikeSender({songid}) {
   );
 }
 
-function CommentSender({songid}) {
+function CommentSender({ songid }) {
   const [comment, SetCommnet] = useState("");
   const onSubmit = async () => {
     const formData = new FormData();
@@ -371,11 +370,9 @@ function CommentSender({songid}) {
         document.getElementById("commentcontent").value = "";
         SetCommnet("");
       }
-    }
-    else if (response.status === 400) {
+    } else if (response.status === 400) {
       toast.error("评论失败：登录了吗？");
-    }
-    else {
+    } else {
       toast.error("评论失败：登录了吗？");
     }
     if (typeof window !== "undefined") {
@@ -410,11 +407,11 @@ function CommentSender({songid}) {
   );
 }
 
-function CommentList({songid}) {
-  const {data, error, isLoading} = useSWR(
+function CommentList({ songid }) {
+  const { data, error, isLoading } = useSWR(
     apiroot3 + "/maichart/" + songid + "/interact",
     fetcher,
-    {refreshInterval: 3000}
+    { refreshInterval: 3000 }
   );
   if (error) {
     return <div>failed to load</div>;
@@ -446,11 +443,11 @@ function CommentList({songid}) {
   return <div className="theList">{objlist}</div>;
 }
 
-function ScoreList({songid}) {
-  const {data, error, isLoading} = useSWR(
+function ScoreList({ songid }) {
+  const { data, error, isLoading } = useSWR(
     apiroot3 + "/maichart/" + songid + "/score",
     fetcher,
-    {refreshInterval: 30000}
+    { refreshInterval: 30000 }
   );
   if (error) {
     return <div>failed to load</div>;
@@ -471,8 +468,7 @@ function ScoreList({songid}) {
       <div className="theList">
         <div className="inputHint">排名</div>
       </div>
-      <div className="theList">{objlist}</div>
-      ;
+      <div className="theList">{objlist}</div>;
     </>
   );
 }
--- src/app/widgets/Majdata.jsx
+++ src/app/widgets/Majdata.jsx
@@ -1,30 +1,54 @@
-
-'use client'
+"use client";
 import { Unity, useUnityContext } from "react-unity-webgl";
-import React from 'react';
+import React from "react";
+import { sleep } from "../utils";
 
-export default function Majdata() {
-    var buildUrl = "/WebGLBuild"; //replace with your CDN
-  
-    const { unityProvider, isLoaded, loadingProgression , sendMessage  } = useUnityContext({
+export default function Majdata({ songid, apiroot, level }) {
+  var buildUrl = "/WebGLBuild"; //replace with your CDN
+
+  const { unityProvider, isLoaded, loadingProgression, sendMessage } =
+    useUnityContext({
       loaderUrl: buildUrl + "/Build.loader.js",
       dataUrl: buildUrl + "/Build.data",
       frameworkUrl: buildUrl + "/Build.framework.js",
       codeUrl: buildUrl + "/Build.wasm",
     });
-  
-    if(isLoaded){
-      //alert("loaded");
-      window.unitySendMessage = sendMessage
-      
-    }
-    
-    return (
-        <div className={'majViewPort'}>
-          <Unity unityProvider={unityProvider} className="majCanvas" style={{ display: isLoaded ? "unset" : "none" }}/>
-          <div className='majPlaceHolder' style={{ display: isLoaded ? "none" : "block" }}>Loading Majdata {`${(loadingProgression * 100).toFixed(2)}% ..`}</div>
-          {/* <button onClick={()=>sendMessage("HandleJSMessages","ReceiveMessage","aaa\n"+apiroot+"\n"+songid+"\nasdasdajdalskdj\n")}>nmsl</button> */}
-        </div>
-      );
-    
+
+  if (isLoaded) {
+    window.unitySendMessage = sendMessage;
+    load()
   }
+
+  async function load() {
+    const maichart = apiroot + "/maichart/" + songid;
+    const maidata = maichart + "/chart";
+    const track = maichart + "/track";
+    const bg = maichart + "/image?fullImage=true";
+    const mv = maichart + "/video";
+    await sleep(500)
+    if (songid !== undefined && apiroot !== undefined && level !== undefined) {
+      sendMessage(
+        "HandleJSMessages",
+        "ReceiveMessage",
+        `${maidata}\n${track}\n${bg}\n${mv}\n${level}`
+      )
+    }
+  }
+
+  return (
+    <div className={"majViewPort"}>
+      <Unity
+        unityProvider={unityProvider}
+        className="majCanvas"
+        style={{ display: isLoaded ? "unset" : "none" }}
+      />
+      <div
+        className="majPlaceHolder"
+        style={{ display: isLoaded ? "none" : "block" }}
+      >
+        Loading Majdata {`${(loadingProgression * 100).toFixed(2)}% ..`}
+      </div>
+      {/* <button onClick={()=>sendMessage("HandleJSMessages","ReceiveMessage","aaa\n"+apiroot+"\n"+songid+"\nasdasdajdalskdj\n")}>nmsl</button> */}
+    </div>
+  );
+}
